1ebfebfe409bc0ec95315b84cd1d155aedbe56f5
A Morrison <geofurlong@outlook.com>
2024-04-14 21:02:22 +0100

    Railway: Improve unit test readability.

--- pkg/geocode/railway_test.go
+++ pkg/geocode/railway_test.go
@@ -8,31 +8,58 @@ func TestValidELR(t *testing.T) {
 	reg := RegexELR()
 
 	cases := []struct {
-		elr      string
-		expected bool
+		elr           string
+		expectedValid bool
 	}{
-		{"NBK", true},
-		{"ECM8", true},
-
-		{"", false},
-		{"e", false},
-		{"E", false},
-		{"EC", false},
-		{"NBk", false},
-		{"nbk", false},
-		{"ecm8", false},
-		{"1234", false},
-		{"EC8M", false},
-		{"ECMx", false},
-		{"ECM1234", false},
-		{"%*3_", false},
+		{
+			elr:           "NBK",
+			expectedValid: true},
+		{
+			elr:           "ECM8",
+			expectedValid: true},
+		{
+			elr:           "",
+			expectedValid: false},
+		{
+			elr:           "e",
+			expectedValid: false},
+		{
+			elr:           "E",
+			expectedValid: false},
+		{
+			elr:           "EC",
+			expectedValid: false},
+		{
+			elr:           "NBk",
+			expectedValid: false},
+		{
+			elr:           "nbk",
+			expectedValid: false},
+		{
+			elr:           "ecm8",
+			expectedValid: false},
+		{
+			elr:           "1234",
+			expectedValid: false},
+		{
+			elr:           "EC8M",
+			expectedValid: false},
+		{
+			elr:           "ECMx",
+			expectedValid: false},
+		{
+			elr:           "ECM1234",
+			expectedValid: false},
+		{
+			elr:           "%*3_",
+			expectedValid: false},
 	}
 
 	for _, c := range cases {
 		res := reg.MatchString(c.elr)
 
-		if res != c.expected {
-			t.Errorf("Valid ELR (%s) = %t; want %t", c.elr, res, c.expected)
+		if res != c.expectedValid {
+			t.Errorf("Valid ELR (%s) = %t; want %t", c.elr, res, c.expectedValid)
 		}
 	}
 
@@ -121,127 +148,268 @@ func TestExplodeTotalYards(t *testing.T) {
 
 func TestBuildTotalYards(t *testing.T) {
 	cases := []struct {
-		miles    int
-		yards    int
-		expected int
+		miles              int
+		yards              int
+		expectedTotalYards int
 	}{
-		{0, -2_884, -2_884}, // ELR HCS: known to be erronous.
-		{0, -9, -9},
-		{0, 0, 0},
-		{0, 2, 2},
-		{0, 1_759, 1_759},
-		{1, 0, 1_760},
-		{1, 3, 1_763},
-		{10, 5, 17_605},
-		{100, 1_759, 177_759},
+		{
+			miles:              0,
+			yards:              -2_884,
+			expectedTotalYards: -2_884}, // ELR HCS: known to be erronous.
+		{
+			miles:              0,
+			yards:              -9,
+			expectedTotalYards: -9},
+		{
+			miles:              0,
+			yards:              0,
+			expectedTotalYards: 0},
+		{
+			miles:              0,
+			yards:              2,
+			expectedTotalYards: 2},
+
+		{miles: 0,
+			yards:              1_759,
+			expectedTotalYards: 1_759},
+		{
+			miles:              1,
+			yards:              0,
+			expectedTotalYards: 1_760},
+		{
+			miles:              1,
+			yards:              3,
+			expectedTotalYards: 1_763},
+		{
+			miles:              10,
+			yards:              5,
+			expectedTotalYards: 17_605},
+		{
+			miles:              100,
+			yards:              1_759,
+			expectedTotalYards: 177_759},
 	}
 
 	for _, c := range cases {
 		res := BuildTotalYards(c.miles, c.yards)
-		if res != c.expected {
-			t.Errorf("BuildTotalYards(%v, %v) = %v, want %v", c.miles, c.yards, res, c.expected)
+		if res != c.expectedTotalYards {
+			t.Errorf("BuildTotalYards(%v, %v) = %v, want %v", c.miles, c.yards, res, c.expectedTotalYards)
 		}
 	}
 }
 
 func TestMetresToMiles(t *testing.T) {
 	cases := []struct {
-		metres   float64
-		expected string
+		metres         float64
+		expectedString string
 	}{
-		{1_609.344 * 0, "0.000 miles"},
-		{1_609.344 * 1, "1.000 miles"},
-		{1_609.344 * 2, "2.000 miles"},
-		{1_609.344 * 9.5, "9.500 miles"},
-		{1_609.344 * 53, "53.000 miles"},
+		{
+			metres:         1_609.344 * 0,
+			expectedString: "0.000 miles"},
+		{
+			metres:         1_609.344 * 1,
+			expectedString: "1.000 miles"},
+		{
+			metres:         1_609.344 * 2,
+			expectedString: "2.000 miles"},
+		{
+			metres:         1_609.344 * 9.5,
+			expectedString: "9.500 miles"},
+		{
+			metres:         1_609.344 * 53,
+			expectedString: "53.000 miles"},
 	}
 
 	for _, c := range cases {
 		res := MetresToMiles(c.metres)
-		if res != c.expected {
-			t.Errorf("MetresToMiles(%v) = %v, want %v", c.metres, res, c.expected)
+		if res != c.expectedString {
+			t.Errorf("MetresToMiles(%v) = %v, want %v", c.metres, res, c.expectedString)
 		}
 	}
 }
 
 func TestFmtTotalYards(t *testing.T) {
 	cases := []struct {
-		requestedTY int
-		metric      bool
-		expected    string
+		requestedTY    int
+		metric         bool
+		expectedString string
 	}{
-		{0, false, "0M 0000y"},
-		{1, false, "0M 0001y"},
-		{1_760, false, "1M 0000y"},
-		{1_760 + 1_759, false, "1M 1759y"},
-		{1_760*35 + 880, false, "35M 0880y"},
+		// Non-metric, positive mileages.
+		{
+			requestedTY:    0,
+			metric:         false,
+			expectedString: "0M 0000y"},
+		{
+			requestedTY:    1,
+			metric:         false,
+			expectedString: "0M 0001y"},
+		{
+			requestedTY:    1_760,
+			metric:         false,
+			expectedString: "1M 0000y"},
+		{
+			requestedTY:    1_760 + 1_759,
+			metric:         false,
+			expectedString: "1M 1759y"},
+		{
+			requestedTY:    1_760*35 + 880,
+			metric:         false,
+			expectedString: "35M 0880y"},
 
-		{-1, false, "0M -001y"},
-		{-2_884, false, "0M -2884y"}, // ELR HCS: known to be erronous.
-		{-1_456, false, "0M -1456y"}, // ELR CJA2.
-		{-965, false, "0M -965y"},    // ELR LFL.
+		// Non-metric, negative mileages.
+		{
+			requestedTY:    -1,
+			metric:         false,
+			expectedString: "0M -001y"},
+		{
+			requestedTY:    -2_884,
+			metric:         false,
+			expectedString: "0M -2884y"}, // ELR HCS: known to be erronous.
+		{
+			requestedTY:    -1_456,
+			metric:         false,
+			expectedString: "0M -1456y"}, // ELR CJA2.
+		{
+			requestedTY:    -965,
+			metric:         false,
+			expectedString: "0M -965y"}, // ELR LFL.
 
-		{0, true, "0.000km"},
-		{1, true, "0.001km"},
-		{1_000, true, "0.914km"},
-		{1_760, true, "1.609km"},
+		// Metric, positive kilometreages.
+		{
+			requestedTY:    0,
+			metric:         true,
+			expectedString: "0.000km"},
+		{
+			requestedTY:    1,
+			metric:         true,
+			expectedString: "0.001km"},
+		{
+			requestedTY:    1_000,
+			metric:         true,
+			expectedString: "0.914km"},
+		{
+			requestedTY:    1_760,
+			metric:         true,
+			expectedString: "1.609km"},
 	}
 
 	for _, c := range cases {
 		res := FmtTotalYards(c.requestedTY, c.metric)
-		if res != c.expected {
-			t.Errorf("FmtTotalYards(%v, %v) = %v, want %v", c.requestedTY, c.metric, res, c.expected)
+		if res != c.expectedString {
+			t.Errorf("FmtTotalYards(%v, %v) = %v, want %v", c.requestedTY, c.metric, res, c.expectedString)
 		}
 	}
 }
 
 func TestFmtMileages(t *testing.T) {
 	cases := []struct {
-		tyFrom   int
-		tyTo     int
-		metric   bool
-		expected string
+		tyFrom         int
+		tyTo           int
+		metric         bool
+		expectedString string
 	}{
-		{0, 0, false, "0M 0000y to 0M 0000y"},
-		{1, 1, false, "0M 0001y to 0M 0001y"},
-		{1_760, 1_760, false, "1M 0000y to 1M 0000y"},
-		{1_760 + 1, 1_760 + 1_759, false, "1M 0001y to 1M 1759y"},
-		{1_760 + 1_759, 1_760 + 1_759, false, "1M 1759y to 1M 1759y"},
-		{1_760*22 + 440, 1_760*33 + 1_320, false, "22M 0440y to 33M 1320y"},
-		{1_760*35 + 880, 1_760*35 + 880, false, "35M 0880y to 35M 0880y"},
-		{0, 0, true, "0.000km to 0.000km"},
-		{1, 1, true, "0.001km to 0.001km"},
-		{1_000, 1_760, true, "0.914km to 1.609km"},
-		{5_468, 6_562, true, "5.000km to 6.000km"},  // 5,000 / 0.9144 = 5,468   6,000 / 0.9144 = 6,562
-		{8_202, 10_116, true, "7.500km to 9.250km"}, // 7,500 / 0.9144 = 8,202   9,250 / 0.9144= 10,116
+		{
+			tyFrom:         0,
+			tyTo:           0,
+			metric:         false,
+			expectedString: "0M 0000y to 0M 0000y"},
+		{
+			tyFrom:         1,
+			tyTo:           1,
+			metric:         false,
+			expectedString: "0M 0001y to 0M 0001y"},
+		{
+			tyFrom:         1_760,
+			tyTo:           1_760,
+			metric:         false,
+			expectedString: "1M 0000y to 1M 0000y"},
+		{
+			tyFrom:         1_760 + 1,
+			tyTo:           1_760 + 1_759,
+			metric:         false,
+			expectedString: "1M 0001y to 1M 1759y"},
+		{
+			tyFrom:         1_760 + 1_759,
+			tyTo:           1_760 + 1_759,
+			metric:         false,
+			expectedString: "1M 1759y to 1M 1759y"},
+		{
+			tyFrom:         1_760*22 + 440,
+			tyTo:           1_760*33 + 1_320,
+			metric:         false,
+			expectedString: "22M 0440y to 33M 1320y"},
+		{
+			tyFrom:         1_760*35 + 880,
+			tyTo:           1_760*35 + 880,
+			metric:         false,
+			expectedString: "35M 0880y to 35M 0880y"},
+		{
+			tyFrom:         0,
+			tyTo:           0,
+			metric:         true,
+			expectedString: "0.000km to 0.000km"},
+		{
+			tyFrom:         1,
+			tyTo:           1,
+			metric:         true,
+			expectedString: "0.001km to 0.001km"},
+		{
+			tyFrom:         1_000,
+			tyTo:           1_760,
+			metric:         true,
+			expectedString: "0.914km to 1.609km"},
+		{
+			tyFrom:         5_468,
+			tyTo:           6_562,
+			metric:         true,
+			expectedString: "5.000km to 6.000km"}, // 5,000 / 0.9144 = 5,468   6,000 / 0.9144 = 6,562
+		{
+			tyFrom:         8_202,
+			tyTo:           10_116,
+			metric:         true,
+			expectedString: "7.500km to 9.250km"}, // 7,500 / 0.9144 = 8,202   9,250 / 0.9144= 10,116
 	}
 
 	for _, c := range cases {
 		res := FmtMileages(c.tyFrom, c.tyTo, c.metric)
-		if res != c.expected {
-			t.Errorf("FmtMileages(%v, %v, %v) = %v, want %v", c.tyFrom, c.tyTo, c.metric, res, c.expected)
+		if res != c.expectedString {
+			t.Errorf("FmtMileages(%v, %v, %v) = %v, want %v", c.tyFrom, c.tyTo, c.metric, res, c.expectedString)
 		}
 	}
 }
 
 func TestFmtTotalYardsMetric(t *testing.T) {
 	cases := []struct {
-		ty       int
-		expected string
+		ty                   int
+		expectedKilometreage string
 	}{
-		{0, "0.000km"},
-		{1_760, "1.609km"},
-		{10_936, "10.000km"}, // 10,000 / 0.9144 = 10,936.133
-		{17_600, "16.093km"},
-		{176_000, "160.934km"},
-		{5_468, "5.000km"},
-		{10_116, "9.250km"},
+		{
+			ty:                   0,
+			expectedKilometreage: "0.000km"},
+		{
+			ty:                   1_760,
+			expectedKilometreage: "1.609km"},
+		{
+			ty:                   10_936,
+			expectedKilometreage: "10.000km"}, // 10,000 / 0.9144 = 10,936.133
+		{
+			ty:                   17_600,
+			expectedKilometreage: "16.093km"},
+		{
+			ty:                   176_000,
+			expectedKilometreage: "160.934km"},
+		{
+			ty:                   5_468,
+			expectedKilometreage: "5.000km"},
+		{
+			ty:                   10_116,
+			expectedKilometreage: "9.250km"},
 	}
 
 	for _, c := range cases {
 		res := FmtTotalYards(c.ty, true)
-		if res != c.expected {
-			t.Errorf("FmtTotalYardsMetric(%d) = %s; want %s", c.ty, res, c.expected)
+		if res != c.expectedKilometreage {
+			t.Errorf("FmtTotalYardsMetric(%d) = %s; want %s", c.ty, res, c.expectedKilometreage)
 		}
 
 	}
